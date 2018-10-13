0949f043b6949d58eb517ff6ce627718bb18e513
Frank <frankhn0801@gmail.com>
2018-10-13 15:59:26 -0600

    added redux

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -2,11 +2,13 @@ import * as React from 'react';
 import './Hello.css';
 
 export interface Props {
-  name: string,
-  enthusiasmLevel?: number; 
+  name: string;
+  enthusiasmLevel?: number;
+  onIncrement?: () => void;
+  onDecrement?: () => void; 
 }
 
-const Hello = ({name, enthusiasmLevel = 0}: Props)  => {
+function Hello({ name, enthusiasmLevel = 1, onIncrement, onDecrement }: Props) {
   if (enthusiasmLevel <= 0) {
     throw new Error('You could be a little more enthusiastic. :D');
   }
@@ -15,6 +17,10 @@ const Hello = ({name, enthusiasmLevel = 0}: Props)  => {
     <div className="hello">
       <div className="greeting">
         Hello {name + getExclamationMarks(enthusiasmLevel)}
+      </div>
+      <div>
+        <button onClick={onDecrement}>-</button>
+        <button onClick={onIncrement}>+</button>
       </div>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,24 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-// import App from './App';
-import StatefulHello from './components/StatefulHello';
+import { Provider } from 'react-redux';
+import Hello from './containers/Hello';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+import { createStore } from 'redux';
+import { enthusiasm } from './reducers/index';
+import { EnthusiasmAction } from './actions/index';
+import { StoreState } from './types/index';
+
+const store = createStore<StoreState, EnthusiasmAction, any, any>(enthusiasm, {
+  enthusiasmLevel: 1,
+  languageName: 'TypeScript',
+});
+
 ReactDOM.render(
-  <StatefulHello name="TypeScript" enthusiasmLevel={1} />,
+  <Provider store={store}>
+    <Hello />
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
